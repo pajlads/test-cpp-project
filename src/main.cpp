567477cc6c89e2f9591179b772659c721823605f
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-08-07 15:48:26 +0200

    1

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,6 @@ int main(int argc, char **argv)
     auto test = new std::string;  // :)
     std::cout << "Hello xD\n";
     auto test2 = new std::string;  // :)
+    auto test3 = new std::string;  // :)
     return 0;
 }
