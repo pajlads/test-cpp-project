3898053606a0ca00f25c4c3756747b66abf95ce6
pajlada <rasmus.karlsson@pajlada.com>
2022-08-07 15:48:08 +0200

    a (#2)

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 
 int main(int argc, char **argv)
 {
-    auto test = new std::string;
+    auto test = new std::string;  // :)
     std::cout << "Hello xD\n";
+    auto test2 = new std::string;  // :)
     return 0;
 }
